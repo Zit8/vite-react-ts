3aeaecf55927a27879d5ddbcaefc92cacb3d0c12
Viktor Kaledintsev <v.kaledinceff@gmail.com>
2023-04-04 22:22:08 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React, { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import elbrusLogo from './assets/elbrus.svg';
-import './App.css';
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
 function App(): JSX.Element {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <h1>Vite by Vitya</h1>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://github.com/Elbrus-Bootcamp" target="_blank" rel="noreferrer">
-          <img src={elbrusLogo} className="logo elbrus" alt="Elbrus logo" />
         </a>
         <a href="https://reactjs.org" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h2>Elbrus Bootcamp</h2>
       <h1>Vite + React</h1>
       <div className="card">
         <button type="button" onClick={() => setCount((prev) => prev + 1)}>
@@ -29,9 +24,9 @@ function App(): JSX.Element {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </div>
   );
 }
-
-export default App;
